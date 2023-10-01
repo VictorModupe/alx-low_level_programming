803169f297a031ec9ad7ac7cb3fe652e2c32e3d1
VictorModupe <Victormodupe007@gmail.com>
2023-10-01 14:19:58 -0700

    power of sleep

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,11 @@
 include "main.h"
 /**
  * _print_rev_recursion - Prints a string in reverse.
- * @s: The string to be printed.
+ * @s: The string to be printed in reverse.
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0') 
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
