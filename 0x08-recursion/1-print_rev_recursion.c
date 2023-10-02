1c22feedef11fe7effed7258749cf56d7eb321d6
VictorModupe <Victormodupe007@gmail.com>
2023-10-02 16:28:31 -0700

    emimo ejo owo mi da

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,21 @@
-include "main.h"
-/**
- * _print_rev_recursion - Prints a string in reverse.
- * @s: The string to be printed in reverse.
+#include <stdio.h>
+/**                                                                                                                 
+ * _print_rev_recursion - Prints a string in reverse.                                                               
+ * @s: The string to be printed in reverse.                                                                         
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0') 
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+    if (*s != '\0')
+    {
+        _print_rev_recursion(s + 1);
+        putchar(*s);
+    }
+}
+
+int main()
+{
+    char str[] = "Hello, World!";
+    _print_rev_recursion(str);
+    putchar('\n');  // Print a newline character at the end
+    return 0;
 }
